ead5783c2b987b3a79b4f6776924c39d13215db3
Rahi <141012258+rahi30-prog@users.noreply.github.com>
2024-07-19 16:07:36 +0530

    Update helloworld.py
    Added meaningful comments.

--- helloworld.py
+++ helloworld.py
@@ -3,9 +3,9 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):   #Get is used to retrieve the data.
         self.send_response(200) #Tells the user that the request is made
-        self.send_header("Content-type", "text/plain")  #Font
-        self.end_headers()  #Ends the header section
-        self.wfile.write(b"Hello, World")   #b", because HTTP communication is done in bytes
+        self.send_header("Content-type", "text/plain")   #use it to specify the content type or length of the response.
+        self.end_headers()  #Ends the header section(MUST be called after all headers have been sent, to complete the operation).
+        self.wfile.write(b"Hello, World")   #b", because HTTP communication is done in bytes. wfile is used for writing a response back to the client
 
 PORT = 8000 #Specifies port number
 
